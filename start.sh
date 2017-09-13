f41684e00e9eaac41f94cf4dc99ef48dae8d1338
varun.dhanraj.mishra <varun.dhanraj.mishra@accenture.com>
2017-09-13 17:33:36 +0530

    Exposed Port 80, Updated jar name

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 spring_boot_jar=weather-app-spring-1.0.0.BUILD-SNAPSHOT-nodb.jar
-chmod +r /tmp/$spring_boot_jar
-sudo cp /tmp/$spring_boot_jar /opt/$spring_boot_jar
-sudo chmod +x /opt/$spring_boot_jar
-sudo sed -i -e  's/abcde/'$spring_boot_jar'/' /tmp/start_spring_boot.sh
+chmod +r /tmp/weather-app-spring-1.0.0.BUILD-SNAPSHOT-nodb.jar
+sudo cp /tmp/weather-app-spring-1.0.0.BUILD-SNAPSHOT-nodb.jar /opt/weather-app-spring-1.0.0.BUILD-SNAPSHOT-nodb.jar
+sudo chmod +x /opt/weather-app-spring-1.0.0.BUILD-SNAPSHOT-nodb.jar
+#sudo sed -i -e  's/abcde/'$spring_boot_jar'/' /tmp/start_spring_boot.sh
 sudo chmod +x /tmp/start_spring_boot.sh
 sudo /tmp/start_spring_boot.sh
--- start_spring_boot.sh
+++ start_spring_boot.sh
@@ -2,7 +2,7 @@
 export JAVA_HOME=/opt/java/jdk1.8.0_131
 export PATH=$PATH:/opt/java/jdk1.8.0_131/bin
 /opt/java/jdk1.8.0_131/bin/java -version
-nohup /opt/java/jdk1.8.0_131/bin/java -jar -Dserver.port=80 /opt/abcde >> /home/ec2-user/nohup.out 2>&1 &
+nohup /opt/java/jdk1.8.0_131/bin/java -jar -Dserver.port=80 /opt/weather-app-spring-1.0.0.BUILD-SNAPSHOT-nodb.jar >> /home/ec2-user/nohup.out 2>&1 &
 sleep 20s
 echo "Server Started"
 echo "Printing Logs..."
